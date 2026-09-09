724f1913323ee712f25f6098a8f1833fa8b6cf6d
KitvisetLabs <kitviset.ai@gmail.com>
2026-09-09 19:17:31 +0700

    Add UQPU compiler and backend implementation v0.2

---
+++ software/uqpu-prototype/uqpu/backends/__init__.py
@@ -0,0 +1,5 @@
+from .base import Backend, BackendAssessment
+from .reversible import ReversibleBackend
+from .quantum_native import QuantumNativeBackend
+
+__all__ = ["Backend", "BackendAssessment", "ReversibleBackend", "QuantumNativeBackend"]
