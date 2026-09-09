c225eebaf9f8577d82d85355f97b96f61ec6cf2f
KitvisetLabs <kitviset.ai@gmail.com>
2026-09-09 19:34:21 +0700

    Add portable execution IR and cloud adapter dry-run layer v0.4

---
+++ software/uqpu-prototype/uqpu/adapters/__init__.py
@@ -0,0 +1,5 @@
+from .ibm import IBMQuantumAdapter
+from .aws_braket import AWSBraketAdapter
+from .azure import AzureQuantumAdapter
+
+__all__ = ["IBMQuantumAdapter", "AWSBraketAdapter", "AzureQuantumAdapter"]
